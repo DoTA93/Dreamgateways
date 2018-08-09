9350f43361240df591648707ec082c84ea1916f5
dhar1993 <38814523+dhar1993@users.noreply.github.com>
2018-08-09 20:25:13 +0530

    updated date and time

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+// (function ($) {
+
+//     // window.onload = function () {
+//     // }
+//     loadWeather("11.617836, 92.723417", "");
+
+
+
+//     function loadWeather(location, woeid) {
+//         $.simpleWeather({
+//             location: location,
+//             unit: 'c',
+//             success: function (weather) {
+
+//                 console.log(weather);
+
+
+//             },
+//             error: function (error) {
+//                 $("#weather").html('<p>' + error + '</p>');
+//             }
+//         });
+//     }
+
+// })(jQuery)
